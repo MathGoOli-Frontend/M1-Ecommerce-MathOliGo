8cb63b3387945689de066bcbbe6c94a6d640b90e
mathGoOli <matheuss456@hotmail.com>
2023-03-17 15:26:17 -0300

    finalizado esperando correção

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
+let userCart = []
+
 // filtros
 // função para filtrar a lista:
-function databaseFilter(data, tag){
+function dataBaseFilter(data, tag){
     if (tag === undefined){
         return data
     }
@@ -8,12 +10,44 @@ function databaseFilter(data, tag){
         return wear.tag.includes(tag)
     })
 }
+// filtro por id
+function dataBaseFilterById(data, id){
+    let newData =[]
+    newData = data.filter(function(wear){
+        return wear.id == id
+    })
+    userCart.push(newData[0])
+}
+
+// filtro remove id
+function dataBaseFilterExcludeById(data, id){
+    userCart = data.filter(function(wear){
+        return !(wear.id == id)
+    })
+    
+}
+
+// filtro por nome e tag
+function dataBaseFilterNameAndTag(search){
+    filterData = []
+    if(search == ''){
+        return data
+    }
+    for(let i = 0; i < data.length; i++){
+        if((data[i].nameItem.toLowerCase().includes(search.toLowerCase())) 
+        || (data[i].tag[0].toLowerCase().includes(search.toLowerCase()))){
+            filterData.push(data[i])
+        }
+    }
+    return filterData
+}
+
 
 //card
 // função para fazer o card
 function createCard(cardInfo){
     const deck = document.getElementsByClassName('deck')[0]
-    console.log(deck)
+    
     //card
     let card = document.createElement('li')
     card.className = 'card'
@@ -37,14 +71,17 @@ function createCard(cardInfo){
     // span price
     let price = document.createElement("span")
     price.className = 'price'
-    price.innerText = 'R$ ' + cardInfo.value + ",00" // não foi implementado os centavos
+    price.innerText = 'R$ ' + cardInfo.value.toFixed(2) // não foi implementado os centavos
     // adicionar ao carrinho
     let addToCart = document.createElement("a")
     addToCart.innerHTML = 'Adicionar ao carrinho'
+    addToCart.id = 'add_' + cardInfo.id
     // função para a ancora
     addToCart.addEventListener('click', function(e){
         e.preventDefault()
-        console.log('teste')
+        let id = e.target.id.substring(4)
+        dataBaseFilterById(data, id)
+        drawCardIten(userCart)
     })
 
     //hierarquia
@@ -62,19 +99,133 @@ function createCard(cardInfo){
     deck.appendChild(card)
 
 }
-createCard({
-    id: 1,
-    img: "./images/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
-    description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  })
 
 // função para apagar/desenhar cards na tela
+function drawSelectedCards(data){
+    const deck = document.getElementsByClassName('deck')[0]
+    deck.innerHTML = ""
 
+    for(let i = 0; i < data.length; i++ ){
+        createCard(data[i])
+    }
+}
 // Carrinho de compras
 // função para fazer o item do carrinho de compras
+function createItem(itemInfo){
+    const cart = document.getElementsByClassName('cart-list')[0]
 
+    // cria o item do carrinho
+    const cartItem = document.createElement('li')
+
+    const imgDiv = document.createElement('div')
+    imgDiv.className = 'cart-img'
+    
+    const img = document.createElement('img')
+    img.src = itemInfo.img
+
+    const infoDiv = document.createElement('li')
+    infoDiv.className = 'item-cart-info'
+    const name = document.createElement('h3')
+    name.className = 'cart-name'
+    name.innerText = itemInfo.nameItem
+    const price = document.createElement('span')
+    price.innerText = "R$" + itemInfo.value.toFixed(2)
+    const removeBtn = document.createElement('a')
+    removeBtn.innerText = "Remover Produto"
+    removeBtn.id = 'remove_' + itemInfo.id
+    removeBtn.addEventListener('click', function(event){
+        event.preventDefault()
+        let id = event.target.id.substring(7)
+        dataBaseFilterExcludeById(userCart, id)
+        
+        drawCardIten(userCart)
+    })
+
+    // montando o item cart
+    cartItem.appendChild(imgDiv)
+    cartItem.appendChild(infoDiv)
+
+    imgDiv.appendChild(img)
+
+    infoDiv.appendChild(name)
+    infoDiv.appendChild(price)
+    infoDiv.appendChild(removeBtn)
+    //
+    
+    cart.appendChild(cartItem)
+
+}
+
+// função para fazer o carrinho vasio
+function emptyCart(){
+    let cart = document.querySelector('aside .cart-title')
+    
+    let div = document.createElement('div')
+    div.className = "cart-empty"
+    div.innerHTML =`
+    <h5>Carrinho Vazio</h5>
+    <p>Adicione Itens</p>`
+
+    cart.after(div)
+
+}
+//função para fazer o carrinho
+function drawCardIten(userCart){
+
+    const contSpan = document.getElementById('cont')
+    let cont = userCart.length
+
+    const priceSpan = document.getElementById('total')
+    let price = 0
+
+    const cart = document.getElementsByClassName("cart-list")[0]
+    cart.innerHTML = ""
+
+    const emptyCartDiv = document.getElementsByClassName("cart-empty")[0]
+    if(emptyCartDiv != undefined){
+        emptyCartDiv.remove()
+    }
+
+    if (userCart.length === 0){
+        emptyCart()
+    }else{
+        for(let i = 0; i < userCart.length; i++){
+            createItem(userCart[i])
+            price += userCart[i].value
+        }
+    }
+    contSpan.innerText = cont
+    priceSpan.innerText = 'R$' + price.toFixed(2)
+}
+
+// run
+
+// Eventos
+let menu = document.querySelectorAll('.header-menu li')
+menu[0].addEventListener('click',function(e){
+    drawSelectedCards(dataBaseFilter(data))
+})
+menu[1].addEventListener('click',function(e){
+    drawSelectedCards(dataBaseFilter(data, "Acessórios"))
+})
+menu[2].addEventListener('click',function(e){
+    drawSelectedCards(dataBaseFilter(data,'Camisetas'))
+})
+
+document.getElementById('searchBtn').addEventListener('click', function(event){
+    let search = document.getElementById("searchBar")
+    console.log(search.value)
+    drawSelectedCards(dataBaseFilterNameAndTag(search.value))
+    
+})
+
+document.getElementById('searchBar').addEventListener('keypress', function(event){
+    let search = document.getElementById("searchBar")
+    console.log(search.value)
+    drawSelectedCards(dataBaseFilterNameAndTag(search.value))  
+
+})
+
+// monta as cartas
+drawSelectedCards(data)
+drawCardIten(userCart)
